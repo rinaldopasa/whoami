5adb531785b344f72374f1c1352088c082e55fe4
Rinaldo Pasa <62827386+rinaldopasa@users.noreply.github.com>
2023-12-11 10:46:34 +0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import { FontaineTransform } from "fontaine";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [],
+  vite: {
+    plugins: [
+      FontaineTransform.vite({
+        fallbacks: ["Arial"],
+        resolvePath: (id) => new URL(`./public${id}`, import.meta.url), // id is the font src value in the CSS
+      }),
+    ],
+  },
+});
